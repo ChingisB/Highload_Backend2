3fd51069225122f8f86a8728ae1c98ec7d33d5da
chingis <chingis.bagdatov@gmail.com>
2024-09-25 20:25:34 +0500

    Add view level caching

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .custom_queries import get_post_with_comments
 from .models import Post
+from django.views.decorators.cache import cache_page
 
 
 def get_post_by_id(request, post_id):
@@ -8,6 +9,8 @@ def get_post_by_id(request, post_id):
     comments = post.comment_set.all()
     return render(request, 'post_with_comments.html', {'post': post, 'comments': comments})
 
+
+@cache_page(60)
 def get_posts(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'post_list.html', {'posts': posts})
